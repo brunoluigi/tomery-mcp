cd9c261aeba3d9e9a44a9245f51a0001a16bf4a7
Bruno Luigi <bruno.luigi@gmail.com>
2025-11-21 13:13:54 +0100

    Fix view spec and add rule to always run specs after tasks
    - Fix recipes/index view spec by properly stubbing current_user
    - Add rule to .cursorrules to always run bin/rspec after completing tasks

--- spec/views/recipes/index.html.tailwindcss_spec.rb
+++ spec/views/recipes/index.html.tailwindcss_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "recipes/index", type: :view do
+  let(:user) { FactoryBot.create(:user) }
+  let(:recipes) { [
+    FactoryBot.create(:recipe, user:),
+    FactoryBot.create(:recipe, user:)
+  ] }
+
   before(:each) do
-    assign(:recipes, [
-      FactoryBot.create(:recipe),
-      FactoryBot.create(:recipe)
-    ])
+    assign(:recipes, recipes)
+    assign(:query, nil)
+    # Define current_user as a helper method for the view
+    test_user = user
+    def view.current_user
+      @current_user
+    end
+    view.instance_variable_set(:@current_user, test_user)
+    allow(test_user).to receive(:pantry_items).and_return(PantryItem.none)
   end
 
   it "renders a list of recipes" do
